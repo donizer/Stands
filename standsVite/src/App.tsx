19209f230f92fd417f468be618847d888940f55e
donizer <bogdan.ua45@gmail.com>
2023-03-24 18:39:42 +0200

    #

--- standsVite/src/App.tsx
+++ standsVite/src/App.tsx
@@ -1,7 +1,4 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import { PixiComp } from "./PixiComp";
 import { PixiComp } from "./PixiComp";
 import "./App.scss";
 import Layout from "./Layout";
